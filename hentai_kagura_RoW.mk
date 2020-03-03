bef40f582a832c1887f9cc12592246ec9a5e440d
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-03-03 00:42:10 -0500

    Update hentai_kagura_RoW.mk

--- hentai_kagura_RoW.mk
+++ hentai_kagura_RoW.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Inherit device parts
-$(call inherit-product, device/sony/kagura/f8331.mk)
+$(call inherit-product, device/sony/kagura/aosp_f8331.mk)
 
 # Inherit Hentai os vendor 
 $(call inherit-product, vendor/hentai/config/common_telephony.mk)
